d4f241f7d6d217c244a3a23d30bce41bd689d27a
Taichi <taichi@curipha.jp>
2015-02-01 17:29:43 +0900

    Add restart script

---
+++ restart.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -u
+set -x
+
+UNICORNPID=`ps aux | grep -E '[u]nicorn master' | awk '{ print $2 }'`
+
+
+sudo service nginx stop
+sudo kill -USR2 ${UNICORNPID}
+
+sudo rm -rf /var/log/nginx/*
+
+sudo service nginx start
+
+sleep 2
+sudo kill -WINCH ${UNICORNPID}
+sleep 2
+sudo kill -QUIT ${UNICORNPID}
+
