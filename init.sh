a97c8389e5253ec5247fa43a270d96dce31d0433
Taichi <taichi@curipha.jp>
2015-02-01 18:26:59 +0900

    Add INDEX to table login_log

--- init.sh
+++ init.sh
@@ -11,3 +11,6 @@ mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/schema.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_users.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
+
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} -e "ALTER TABLE login_log ADD INDEX ip (ip)"
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} -e "ALTER TABLE login_log ADD INDEX user_id (user_id)"
